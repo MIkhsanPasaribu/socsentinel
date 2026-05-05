0f9b7c4605ed5bf90bcb4b4d7fcfdb5cdce06bc0
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2026-05-06 02:01:55 +0700

    chore(frontend): konfigurasi linting dan integrasi toast provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@
 import { RouterProvider } from "react-router-dom";
 import { Providers } from "./core/providers/Providers";
 import { router } from "./core/router/router";
+import { ToastProvider } from "./shared/components/Toast";
 
 function App() {
   return (
     <Providers>
-      <RouterProvider router={router} />
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
     </Providers>
   );
 }
